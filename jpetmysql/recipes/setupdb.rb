3552c36fa98fd7dc398889aba01a6f87900b3a52
Naoto TAKAHASHI <t.naoto@jp.fujitsu.com>
2014-05-09 18:15:40 +0900

    fix SQL dataload

--- jpetmysql/recipes/setupdb.rb
+++ jpetmysql/recipes/setupdb.rb
@@ -11,13 +11,7 @@ node[:deploy].each do |app_name, deploy|
   mysql_command = "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}"
 
   execute "execute-query" do
-     command "#{mysql_command} < /tmp/jpetstore-mysql-schema.sql"
-     not_if "#{mysql_command} -e 'SHOW TABLES' | grep #{deploy[:database][:database]}"
-     action :run
-  end
-
-  execute "execute-query-load" do
-     command "#{mysql_command} < /tmp/jpetstore-mysql-dataload.sql"
+     command "#{mysql_command} < /tmp/jpetstore-mysql-schema.sql && #{mysql_command} < /tmp/jpetstore-mysql-dataload.sql"
      not_if "#{mysql_command} -e 'SHOW TABLES' | grep #{deploy[:database][:database]}"
      action :run
   end
